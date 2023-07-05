5c0fc41c76d8fdc0fe78bd0bad492f81453b6ec1
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 22:14:28 +0530

    com

--- Unit-Converter/convert.c
+++ Unit-Converter/convert.c
@@ -18,15 +18,16 @@ int main()
     int ms, sec, min, hrs;
     int cg, mg, kg;
     int wt, kw, js;
-    int unit;
+    char unit;
+    char from, to;
 
-    // printf("This calculator converts units: \n");
-    // printf("of Temperature(T): \n Celcius(C)\n Farhenheit(F)\n Kelvin(k)\n");
-    // printf("of Distance(d): \n CentiMeter(cm)\n MilliMeter(mm)\n KiloMeter(km)\n");
-    // printf("of Volume(v): \n Litre(l)\n MilliLitre(ml)\n KiloLitre(kl)\n");
-    // printf("of Time(tm): \n MilliSeconds(ms)\n Seconds(sec)\n Minutes(min)\n Hours(hrs)\n");
-    // printf("of Weight(w): \n CentiGram(cg)\n MilliGram(mg)\n KiloGram(kg)\n");
-    // printf("of Power(p): \n Watt(wt)\n kiloWatt(kw)\n Joules(js)\n");
+    printf("This calculator converts units: \n");
+    printf("of Temperature(T): \n Celcius(C)\n Farhenheit(F)\n Kelvin(k)\n");
+    printf("of Distance(d): \n CentiMeter(cm)\n MilliMeter(mm)\n KiloMeter(km)\n");
+    printf("of Volume(v): \n Litre(l)\n MilliLitre(ml)\n KiloLitre(kl)\n");
+    printf("of Time(tm): \n MilliSeconds(ms)\n Seconds(sec)\n Minutes(min)\n Hours(hrs)\n");
+    printf("of Weight(w): \n CentiGram(cg)\n MilliGram(mg)\n KiloGram(kg)\n");
+    printf("of Power(p): \n Watt(wt)\n kiloWatt(kw)\n Joules(js)\n");
 
     printf("Enter what do you want to convert: \n");
     scanf("%c", &unit);
@@ -37,53 +38,54 @@ int main()
     }
     else if (unit == 'd')
     {
-        Distance(cm, mm, km);
+        //   Distance(cm, mm, km);
     }
     else if (unit == 'v')
     {
-        Volume(l, ml, kl);
+        // Volume(l, ml, kl);
     }
     else if (unit == 't')
     {
-        Time(ms, sec, hrs, min);
+        // Time(ms, sec, hrs, min);
     }
     else if (unit == 'w')
     {
-        Weight(cg, mg, kg);
+        // Weight(cg, mg, kg);
     }
     else if (unit == 'p')
     {
-        Power(wt, kw, js);
+        // Power(wt, kw, js);
     }
 }
 
 int Temperature(int C, int F, int K)
 {
-    int val1;
-    int val2;
-    printf("You want to convert a unit of Temperature");
-    printf("Enter what you want to convert FROM:\n");
-    scanf("%c\n", &val1);
-    printf("Enter what you want to convert TO:\n");
-    scanf("%c\n", &val2);
-}
 
-int Distance(int cm, int mm, int km)
-{
-}
+    char from, to;
+    
+    printf("Enter what you want to convert from: \n");
+    scanf("%c", &from);
 
-int Volume(int kl, int ml, int l)
-{
-}
+    if (from == 'C')
+    {
+        printf("Enter what do you want to convert to: \n");
+        scanf("%c", &to);
+        printf("Enter calur of unit: \n");
+        scanf("%d", &C);
 
-int Time(int ms, int sec, int min, int hrs)
-{
-}
-
-int Weight(int cg, int mg, int kg)
-{
-}
-
-int Power(int wt, int kw, int js)
-{
+        if (to == 'K')
+        {
+            K = C + 273;
+            printf("Result is %d", K);
+        } else if (to == 'F'){
+            F = C*(9/5)+32;
+            printf("Result is %d", F);
+        }
+    }
+    else if (from == 'K')
+    {
+    }
+    else if (from == 'F')
+    {
+    }
 }
