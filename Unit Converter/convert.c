48eed323311124d2aac616e9a322b181be01dcbd
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 19:40:14 +0530

    convert

--- Unit Converter/convert.c
+++ Unit Converter/convert.c
@@ -3,6 +3,14 @@
 #include <string.h>
 
 int main(){
-    
+    int t, d, v, s, w, p;
+    int C, F, K;
+    int cm, mm, km;
+    int cg, mg, kg;
+    int kl, ml, l;
+    int ms, s, min, hrs;
+
+    printf("This calculator converts units of Temperature(t)\:n Celcius(C)\n Farhenheit(F)\n Kelvin(k)\n" );
+    printf("");
 }
 
