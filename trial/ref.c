6a2f7b80069a240b44d3679a243c810d3b1209b0
Somya Jha <sumicode.exe@gmail.com>
2023-07-15 16:00:29 +0530

    try and ref works

---
+++ trial/ref.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+    // declaration
+    char name[] = "Somya Jha";
+    char course[] = {'m', 'a', 't', 'h', 's', ' ', 'c', 'o', 'm', 'p', '\0'};
+    char firstName[] = "Somya";
+    char lastName[] = "Jha";
+
+    char str[100];
+    char ch;
+    int i = 0;
+
+    while (ch != '\n')
+    {
+        printf("enter a string: ");
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++;
+    }
+    str[i] = '\0';
+    puts(str);
+    
+    // input a string
+    // char firstName[40];
+    // printf("Enter first name : ");
+    // scanf("%s", firstName);
+    // printf("Your first name is %s\n", firstName);
+
+    // char fullName[40];
+    // printf("Enter full name : ");
+    // scanf("%s", fullName);
+    // printf("Your full name is %s\n", fullName);
+
+    // THIS IS THE PROBLEM WITH scanf() AND printf()
+    // IT ONLY CONSIDERS CHARACTERS UPTO SPACE
+    // THAT IS WHY WE USE fgets() AND puts()
+
+    // gets() & puts()
+    char fullName[40];
+    printf("Enter full name : ");
+    fgets(fullName, 40, stdin);
+    puts(fullName);
+    printf("After reversing string is =%s", strrev(fullName));
+}
--- trial/try.c
+++ trial/try.c
@@ -1,8 +1,36 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char monday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+//     char tuesday[40] = {'t', 'u', 'e', 's', 'd', 'a', 'y', '\0'};
+//     char wednesday[40] = {'w', 'e', 'd', 'n', 'e', 's', 'd', 'a', 'y', '\0'};
+//     char thursday[40] = {'t', 'h', 'u', 'r', 's', 'd', 'a', 'y', '\0'};
+//     char saturday[40] = {'s', 'a', 't', 'u', 'r', 'd', 'a', 'y', '\0'};
+//     char sunday[40] = {'s', 'u', 'n', 'd', 'a', 'y', '\0'};
+//     char friday[40] = {'f', 'r', 'i', 'd', 'a', 'y', '\0'};
+
+//     char whatDay[40];
+//     fgets(whatDay, 40, stdin);
+//     puts(whatDay);
+
+//     // if(strcmp(whatDay, monday) == 0){
+//     //     printf("Monday");
+//     // }
+// }
+
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
 
 int main()
 {
+    // declaration
+    char str[100];
+    char ch;
+    int i = 0;
+
     char monday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
     char tuesday[40] = {'t', 'u', 'e', 's', 'd', 'a', 'y', '\0'};
     char wednesday[40] = {'w', 'e', 'd', 'n', 'e', 's', 'd', 'a', 'y', '\0'};
@@ -10,4 +38,17 @@ int main()
     char saturday[40] = {'s', 'a', 't', 'u', 'r', 'd', 'a', 'y', '\0'};
     char sunday[40] = {'s', 'u', 'n', 'd', 'a', 'y', '\0'};
     char friday[40] = {'f', 'r', 'i', 'd', 'a', 'y', '\0'};
+
+    // gets() & puts()
+    char fullName[40];
+    printf("Enter full name : ");
+    fgets(fullName, 40, stdin);
+    puts(fullName);
+    printf("After reversing string is =%s", strrev(fullName));
+
+    if(strcmp(fullName, monday) != 0){
+        printf("Monday");
+    } else {
+        printf("no");
+    }
 }
