e12df247c9a624d5bac84bdf669140872d4ec74d
Somya Jha <sumicode.exe@gmail.com>
2023-07-15 15:50:05 +0530

    dow

---
+++ trial/try.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char monday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+    char tuesday[40] = {'t', 'u', 'e', 's', 'd', 'a', 'y', '\0'};
+    char wednesday[40] = {'w', 'e', 'd', 'n', 'e', 's', 'd', 'a', 'y', '\0'};
+    char thursday[40] = {'t', 'h', 'u', 'r', 's', 'd', 'a', 'y', '\0'};
+    char saturday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+    char sunday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+    char friday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+}
