e737179388ec9f7e3ab506d95daa608de49946cb
Somya Jha <sumicode.exe@gmail.com>
2023-07-15 15:50:42 +0530

    dow2

--- trial/try.c
+++ trial/try.c
@@ -7,7 +7,7 @@ int main()
     char tuesday[40] = {'t', 'u', 'e', 's', 'd', 'a', 'y', '\0'};
     char wednesday[40] = {'w', 'e', 'd', 'n', 'e', 's', 'd', 'a', 'y', '\0'};
     char thursday[40] = {'t', 'h', 'u', 'r', 's', 'd', 'a', 'y', '\0'};
-    char saturday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
-    char sunday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
-    char friday[40] = {'m', 'o', 'n', 'd', 'a', 'y', '\0'};
+    char saturday[40] = {'s', 'a', 't', 'u', 'r', 'd', 'a', 'y', '\0'};
+    char sunday[40] = {'s', 'u', 'n', 'd', 'a', 'y', '\0'};
+    char friday[40] = {'f', 'r', 'i', 'd', 'a', 'y', '\0'};
 }
