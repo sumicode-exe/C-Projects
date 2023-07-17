b654e33ca9fb0eadf0af409bf542ab06734eaa9c
Somya Jha <sumicode.exe@gmail.com>
2023-07-17 19:45:16 +0530

    main

--- Unit-Converter/convert.c
+++ Unit-Converter/convert.c
@@ -3,3 +3,14 @@
 #include <string.h>
 
 int Temperature(float Cel, float Far, float Kel, float Del, float Rom, float New, float Ran, float Rea);
+
+int main(){
+    printf("Enter what you want to convert:");
+
+    char unit;
+    scnaf("%s", unit);
+
+    if((strcmp(unit, "temperature") == 0)){
+        
+    }
+}
