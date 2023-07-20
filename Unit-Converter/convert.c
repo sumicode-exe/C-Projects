c7bae4937b8190eb90f80d5059c0231052dee355
Somya Jha <sumicode.exe@gmail.com>
2023-07-21 00:08:38 +0530

    //

--- Unit-Converter/convert.c
+++ Unit-Converter/convert.c
@@ -235,13 +235,13 @@ int Time()
     {
         printf("You want to convert %lf %s", value, scale);
 
-        seconds = value / 10;
-        minutes = value / (60 * 60);
-        hours = value / (60 * 60 * 60);
-        days = value / (60 * 60 * 60 * 24);
-        weeks = value / (60 * 60 * 60 * 24 * 7);
-        months = value / (60 * 60 * 60 * 24 * 7 * 4.2);
-        years = value / (60 * 60 * 60 * 24 * 7 * 4.2 * 12);
+        // seconds = value / 10;
+        // minutes = value / (60 * 60);
+        // hours = value / (60 * 60 * 60);
+        // days = value / (60 * 60 * 60 * 24);
+        // weeks = value / (60 * 60 * 60 * 24 * 7);
+        // months = value / (60 * 60 * 60 * 24 * 7 * 4.2);
+        // years = value / (60 * 60 * 60 * 24 * 7 * 4.2 * 12);
 
         printf("%6f seconds \n", seconds);
         printf("%6f minutes \n", minutes);
