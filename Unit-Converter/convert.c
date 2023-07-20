9a13516b7a8468f80dd54e19daa95c0f0d14d1dc
Somya Jha <sumicode.exe@gmail.com>
2023-07-21 00:20:14 +0530

    ent

--- Unit-Converter/convert.c
+++ Unit-Converter/convert.c
@@ -149,6 +149,7 @@ int Temperature()
         printf("%6f days \n", Newton);
         printf("%6f weeks \n", Rankine);
         printf("%6f months \n", Reamur);
+        
         printf("%6f years \n", Farenheit);
     }
     else
