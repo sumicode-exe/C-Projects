61f5c9dbe60647e5cafe6fe4b10a202035482ccb
Somya Jha <sumicode.exe@gmail.com>
2023-07-21 00:35:15 +0530

    vol

--- Unit-Converter/volume.h
+++ Unit-Converter/volume.h
@@ -2,3 +2,39 @@
 #include <math.h>
 #include <string.h>
 
+int Volume();
+
+int Volume()
+{
+    float millilitres, centilitres;
+    float kilolitres, litres;
+
+    double value;
+    char scale[20];
+
+    printf("Enter the calue you want to convert [in the form: {number <enter> scale}] \n");
+    printf("[PLEASE W/O CAPS:] \n");
+    scanf("%lf %s", &value, scale);
+
+    if ((strcmp(scale, "millilitres") == 0))
+    {
+        printf("You want to convert %lf %s", value, scale);
+    }
+    else if ((strcmp(scale, "centilitres") == 0))
+    {
+        printf("You want to convert %lf %s", value, scale);
+    }
+    else if ((strcmp(scale, "kilolitres") == 0))
+    {
+        printf("You want to convert %lf %s", value, scale);
+    }
+    else if ((strcmp(scale, "litres") == 0))
+    {
+        printf("You want to convert %lf %s", value, scale);
+    }
+    else
+    {
+        printf("Please enter a valid scale of Conversion \n");
+        printf("Check the spelling and make sure to write in all small");
+    }
+}
